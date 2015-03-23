22ded157df594d9b605b235055fe01acdf35068a
drxwat <mn@it25.org>
2015-03-23 10:28:07 +1000

    better structure TODOs

---
+++ src/ServerInterface.java
@@ -0,0 +1,8 @@
+/**
+ * TODO: Дополнить интерфейс и создать абстрактник с поддержкой событий
+ * TODO: Создать интерфейст и абстрактник для Клиентов (IO, NOI, NIO2)
+ * Created by drxwat on 23.03.15.
+ */
+public interface ServerInterface {
+
+}
